90bac8dac52e4dfc7d1980cb1e2ba91fe9961612
VinitDamani <129088128+VinitDamani@users.noreply.github.com>
2023-07-24 16:17:42 +0530

    Delete CSV-Upload-File-Nodejs-main directory

--- CSV-Upload-File-Nodejs-main/CSV-Upload-File-Nodejs-main/config/middleware.js
+++
@@ -1,9 +0,0 @@
-//middleware to set res locals flash
-module.exports.setFlash = (req, res, next) => {
-  res.locals.flash = {
-    'success' : req.flash('success'),
-    'error': req.flash('error')
-  }
-
-  next();
-}
